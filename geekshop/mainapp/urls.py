2e340ec1d451aa6b8a62aa00f036272fc10289bc
vit58 <dvit58@yandex.ru>
2021-11-15 09:18:13 +0300

    Ex_8

---
+++ geekshop/mainapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from mainapp import views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.products, name='products'),
+    path('category/<int:pk>/', mainapp.products, name='category_page'),
+    path('category/<int:pk>/<int:page>/', mainapp.products, name='category_page'),
+    path('product/<int:pk>', mainapp.product, name='product')
+]
